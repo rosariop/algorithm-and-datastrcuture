1bdf4b739797c6387ea76e3e34e18a5e216cff03
rosario polito <rosario.polito@gmx.de>
2023-07-18 15:10:37 +0200

    renaming and implementing queue

---
+++ customQueue.py
@@ -0,0 +1,30 @@
+def enqueue(queue, element):
+    queue.insert(0, element)
+
+def dequeue(queue):
+    try:
+        queue.remove(queue[-1])
+    except:
+        print('nothing to  from an empty queue')
+
+def viewQueue(queue):
+    try:
+        print(queue[-1])
+    except:
+        print('nothing to read from an empty queue')
+    
+
+queue = [1,2,3]
+
+print(queue)
+enqueue(queue, 4)
+print(queue)
+dequeue(queue)
+print(queue)
+dequeue(queue)
+print(queue)
+dequeue(queue)
+print(queue)
+dequeue(queue)
+print(queue)
+dequeue(queue)
