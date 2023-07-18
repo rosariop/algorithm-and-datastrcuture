0a2ed6bac3d2aca127ce7ff193df9eddbe52ccd1
rosario polito <rosario.polito@gmx.de>
2023-07-18 15:37:18 +0200

    reformatting code

--- stack.py
+++ stack.py
@@ -1,11 +1,13 @@
 def view(stack):
-        try:
-            return stack[-1]
-        except:
-            print('nothing to read from an empty stack.')
+    try:
+        return stack[-1]
+    except:
+        print('nothing to read from an empty stack.')
+
 
 def push(stack, element):
     stack.append(element)
+
 
 def pop(stack):
     try:
@@ -13,4 +15,17 @@ def pop(stack):
     except:
         print('nothing to delete from an empty stack')
 
+
 stack = []
+
+push(stack, 1)
+push(stack, 2)
+push(stack, 3)
+
+print(stack)
+pop(stack)
+pop(stack)
+pop(stack)
+print(stack)
+
+pop(stack)
