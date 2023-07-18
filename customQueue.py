537a6efdd9c58f78a6a0863d41347481be2ac66d
rosario polito <rosario.polito@gmx.de>
2023-07-18 18:27:23 +0200

    reformatting customQueue.py

--- customQueue.py
+++ customQueue.py
@@ -1,20 +1,22 @@
 def enqueue(queue, element):
     queue.insert(0, element)
 
+
 def dequeue(queue):
     try:
         queue.remove(queue[-1])
     except:
         print('nothing to  from an empty queue')
 
+
 def viewQueue(queue):
     try:
         print(queue[-1])
     except:
         print('nothing to read from an empty queue')
-    
 
-queue = [1,2,3]
+
+queue = [1, 2, 3]
 
 print(queue)
 enqueue(queue, 4)
